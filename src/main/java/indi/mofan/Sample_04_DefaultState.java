4a89a14b9467588529da809a9437761d99275bda
mofan <cy.mofan@foxmail.com>
2023-12-18 23:33:36 +0800

    Default State

---
+++ src/main/java/indi/mofan/Sample_04_DefaultState.java
@@ -0,0 +1,34 @@
+package indi.mofan;
+
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.Warmup;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+@State(Scope.Thread)
+@Warmup(iterations = 1, time = 1)
+@Measurement(iterations = 1, time = 1)
+public class Sample_04_DefaultState {
+
+    double x = Math.PI;
+
+    @Benchmark
+    public void measure() {
+        x++;
+    }
+
+    public static void main(String[] args) throws RunnerException {
+        Options opt = new OptionsBuilder()
+                .include(Sample_04_DefaultState.class.getSimpleName())
+                .forks(1)
+                .build();
+
+        new Runner(opt).run();
+    }
+
+}
